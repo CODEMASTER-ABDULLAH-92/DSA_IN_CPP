17185cfad8ad2dd32437150754cce7be33786b00
CODEMASTER-ABDULLAH-92 <abdullahsabir9245@gmail.com>
2024-10-24 11:42:12 +0500

    Adding Key Points in Bubble Sort Algo

--- Array/BubbleSort.cpp
+++ Array/BubbleSort.cpp
@@ -1,3 +1,8 @@
+//In bubble sort we compare with the neighbour index element, But in selection Sort Compare with the next whole remaining array
+//Not If a > b then Swapping
+
+
+//Use Case :: In every ith round we shiftted the ith largest element at the right side
 #include<iostream>
 using namespace std;
 void printArr(int arr[], int n){
@@ -8,7 +13,9 @@ void printArr(int arr[], int n){
 }
 void bubbldde(int arr[], int n){
     for(int i =0; i< n; i++){
+        //If we started the outer loop from int i = 1; i< n; then the inner loop condition is j<n-1; and if we use the int j =1; then use the j < n - i -1 ;
         for(int j = 0; j < n - i -1; j++){
+            //Use the n - i instead of n-1; because the last element is Sorted;
             if(arr[j] > arr[j + 1]){
                 swap(arr[j], arr[j + 1]);
             }
