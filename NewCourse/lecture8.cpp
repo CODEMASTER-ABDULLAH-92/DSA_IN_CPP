54a24f39b305e3ac55a22f5f7442ab4c01212e9e
CODEMASTER-ABDULLAH-92 <abdullahsabir9245@gmail.com>
2025-01-26 07:53:17 +0500

    "Adding the Patterns "

---
+++ NewCourse/lecture8.cpp
@@ -0,0 +1,76 @@
+//         * 
+//       * * 
+//     * * * 
+//   * * * * 
+// * * * * * 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n =5;
+//     for(int row = 1; row<=5; row++){
+//         for(int col = 1; col<= n - row; col++){
+//             cout<<"  ";
+//         }
+//         for(int col = 1; col<=row; col++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }   
+// return 0;
+// }
+
+
+//       1 
+//     2 2 
+//   3 3 3 
+// 4 4 4 4 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  int n = 4;
+//  for(int row = 1; row<=n; row++){
+//     for(int col = 1; col<= n - row; col++){
+//         cout<<"  ";
+//     }
+//     for(int col = 1; col<=row; col++){
+//         cout<<row<<" ";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+
+//         1 
+//       1 2 
+//     1 2 3 
+//   1 2 3 4 
+// 1 2 3 4 5 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//  for(int row = 1; row<=5; row++){
+//     for(int col = 1; col<= 5 - row; col++ ){
+//         cout<<"  ";
+//     }
+//     for(int col = 1; col<=row; col++){
+//         cout<<col<<" ";
+//     }
+//     cout<<endl;
+//  }   
+// return 0;
+// }
+
+
+
+// A 
+// B A 
+// C B A 
+// D C B A 
+// E D C B A 
+
+
+// ============== Code is Pending for this Pattren 
